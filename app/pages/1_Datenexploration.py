38ad025dcd732ad14df64fe6efe5bb636dca4bcd
Korff, Matti Gerrit <matti.gerrit.korff@exxeta.com>
2022-11-10 11:25:45 +0100

    Adjust formatting

--- app/pages/1_Datenexploration.py
+++ app/pages/1_Datenexploration.py
@@ -6,6 +6,86 @@ import streamlit as st
 sns.set_style("darkgrid")
 sns.set_palette("tab10")
 
+
+def hist_plot(
+    col: str, bins: int, kde: bool = False, log_scale: bool = False, hue: str = None
+):
+
+    fig, ax = plt.subplots()
+
+    hue_values = df[hue].astype(str) if hue is not None else None
+
+    sns.histplot(
+        df,
+        x=col,
+        bins=bins,
+        ax=ax,
+        kde=kde,
+        stat="count",
+        log_scale=log_scale,
+        hue=hue_values,
+    )
+    min_ylim, max_ylim = ax.get_ylim()
+    min_xlim, max_xlim = ax.get_xlim()
+    x_range = abs(max_xlim - min_xlim)
+    ax.axvline(df[col].mean(), color="k", linestyle="dashed", linewidth=1)
+    ax.text(
+        df[col].mean() + x_range * 0.02,
+        max_ylim * 0.9,
+        "Mean: {:.2f}".format(df[col].mean()),
+    )
+    ax.axvline(df[col].median(), color="k", linestyle="solid", linewidth=1)
+    ax.text(
+        df[col].median() + x_range * 0.02,
+        max_ylim * 0.8,
+        "Median: {:.2f}".format(df[col].median()),
+    )
+    ax.set_xlabel(col)
+
+    return fig
+
+
+def box_plot(col: str, show_outlier: bool = True):
+
+    fig, ax = plt.subplots()
+    fig_data = ax.boxplot(
+        df[col],
+        bootstrap=1000,
+        autorange=True,
+        showmeans=True,
+        showfliers=show_outlier,
+    )
+
+    return fig
+
+
+def scatter_plot(
+    scatter_x: str,
+    scatter_y: str,
+    scatter_color: str,
+    scatter_size: str,
+    alpha: float,
+):
+
+    fig, ax = plt.subplots()
+
+    if scatter_color is None:
+        hue_values = None
+    else:
+        hue_values = scatter_color
+
+    sns.scatterplot(
+        data=df,
+        x=scatter_x,
+        y=scatter_y,
+        alpha=alpha,
+        hue=hue_values,
+        size=scatter_size,
+        ax=ax,
+    )
+    return fig
+
+
 st.set_page_config(page_title="Datenverständnis")
 
 st.markdown("# Datenverständnis")
@@ -37,62 +117,12 @@ else:
     kde = st.checkbox("Dichteschätzer", value=False)
     log_scale = st.checkbox("Log. Skalierung", value=False)
 
-    def hist_plot(
-        col: str, bins: int, kde: bool = False, log_scale: bool = False, hue: str = None
-    ):
-
-        fig, ax = plt.subplots()
-
-        hue_values = df[hue].astype(str) if hue is not None else None
-
-        sns.histplot(
-            df,
-            x=col,
-            bins=bins,
-            ax=ax,
-            kde=kde,
-            stat="count",
-            log_scale=log_scale,
-            hue=hue_values,
-        )
-        min_ylim, max_ylim = ax.get_ylim()
-        min_xlim, max_xlim = ax.get_xlim()
-        x_range = abs(max_xlim - min_xlim)
-        ax.axvline(df[col].mean(), color="k", linestyle="dashed", linewidth=1)
-        ax.text(
-            df[col].mean() + x_range * 0.02,
-            max_ylim * 0.9,
-            "Mean: {:.2f}".format(df[col].mean()),
-        )
-        ax.axvline(df[col].median(), color="k", linestyle="solid", linewidth=1)
-        ax.text(
-            df[col].median() + x_range * 0.02,
-            max_ylim * 0.8,
-            "Median: {:.2f}".format(df[col].median()),
-        )
-        ax.set_xlabel(col)
-
-        return fig
-
     st.pyplot(hist_plot(feature, bins, kde, log_scale, hue_var))
 
     st.sidebar.markdown("#### [Box Plots](#box-plots) ")
     st.write("### Box Plots")
 
     show_outlier = st.checkbox("Zeige Outlier", value=True)
-
-    def box_plot(col: str, show_outlier: bool = True):
-
-        fig, ax = plt.subplots()
-        fig_data = ax.boxplot(
-            df[col],
-            bootstrap=1000,
-            autorange=True,
-            showmeans=True,
-            showfliers=show_outlier,
-        )
-
-        return fig
 
     st.pyplot(box_plot(feature, show_outlier))
 
@@ -125,31 +155,5 @@ else:
     )
     widget_key += 1
     alpha = st.slider("Alpha", min_value=0.01, max_value=1.0, value=0.7)
-
-    def scatter_plot(
-        scatter_x: str,
-        scatter_y: str,
-        scatter_color: str,
-        scatter_size: str,
-        alpha: float,
-    ):
-
-        fig, ax = plt.subplots()
-
-        if scatter_color is None:
-            hue_values = None
-        else:
-            hue_values = scatter_color
-
-        sns.scatterplot(
-            data=df,
-            x=scatter_x,
-            y=scatter_y,
-            alpha=alpha,
-            hue=hue_values,
-            size=scatter_size,
-            ax=ax,
-        )
-        return fig
 
     st.pyplot(scatter_plot(scatter_x, scatter_y, scatter_color, scatter_size, alpha))
