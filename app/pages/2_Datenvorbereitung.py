0008b2661580c7f819a6535150dadcb5c42d9a6c
Korff, Matti Gerrit <matti.gerrit.korff@exxeta.com>
2022-11-10 13:48:14 +0100

    Add Data preperation

--- app/pages/2_Datenvorbereitung.py
+++ app/pages/2_Datenvorbereitung.py
@@ -4,14 +4,32 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 
-labelencoder = LabelEncoder()
-
 st.set_page_config(page_title="Datenvorbereitung")
 
 st.markdown("# Datenvorbereitung")
 st.sidebar.header("Datenvorbereitung")
 
-if "housing_data" not in st.session_state:
-    st.session_state["housing_data"] = pd.read_csv("data/housing.csv")
 
-housing_data = st.session_state["housing_data"]
+def box_plot(col: str, show_outlier: bool = True):
+    fig, ax = plt.subplots()
+    fig_data = ax.boxplot(
+        df[col],
+        bootstrap=1000,
+        autorange=True,
+        showmeans=True,
+        showfliers=show_outlier,
+    )
+
+    return fig
+
+
+if "dataset" not in st.session_state:
+    st.error("Bitte zuerst einen Datensatz auswählen!")
+    st.markdown("[Datenauswahl](Datenauswahl)")
+else:
+    dataset = st.session_state["dataset"]
+    df = dataset.data
+
+    st.write("### Normalization")
+    st.dataframe(df.head())
+    st.pyplot(box_plot(df.columns, False))
