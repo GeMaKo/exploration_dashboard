ece2f5cadcc8111af3d98f79590ea8e98c5849cf
MaGeKo-Exxeta <matti.gerrit.korff@exxeta.com>
2023-09-19 08:15:28 +0200

    Feat: Add titanic dataset

---
+++ src/mlcook/datasets/titanic.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import seaborn as sns
+
+from .base import Dataset
+
+
+class IrisDataset(Dataset):
+    name = "Titanic"
+
+    def _load_data(self):
+        df: pd.DataFrame = sns.load_dataset("titanic")(as_frame=True)
+        df = df.rename(columns={"target": "class"})
+        y = "survived"
+        self.data = df
+        self.X = df.drop(columns=[y])
+        self.y = df["survived"]
+        self.target = "survived"
+        self.descr = ""
+
+    @property
+    def categorical_features(self):
+        return (
+            "pclass",
+            "sex",
+            "embarked",
+            "class",
+            "who",
+            "adult_male",
+            "embark_town",
+            "alive",
+            "alone",
+        )
+
+    @property
+    def numerical_features(self):
+        return tuple(set(self.data.columns) - set(self.categorical_features))
+
+    def is_regression(self):
+        return False
